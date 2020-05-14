13397536dd2476ba3c73b107ad6044b773c84f25
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-14 22:52:43 +0300

    init

--- ScrollButtonUpWithKeyboard/ViewController.swift
+++ ScrollButtonUpWithKeyboard/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tbnButtomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
     }
-
+    
+    
+    @objc func keyboardHide(notification: Notification) {
+        if let frame = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let height = frame.cgRectValue.height
+            UIView.animate(withDuration: 0.5) {
+                self.tbnButtomConstraint.constant = height - height + 20
+            }
+        }
+    }
+    
+    @objc func keyboardShow(notification: Notification) {
+        if let frame = notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let height = frame.cgRectValue.height
+            UIView.animate(withDuration: 0.5) {
+                self.tbnButtomConstraint.constant = height + 20
+            }
+            
+        }
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
 
 }
 
